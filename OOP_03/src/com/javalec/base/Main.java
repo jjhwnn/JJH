8d21feb17bfab2a335dc8b7ca14695f8528ec1ed
jjhwnn <jeahwan174@gmail.com>
2022-03-02 12:58:34 +0900

    Update OOP_03

--- OOP_03/src/com/javalec/base/Main.java
+++ OOP_03/src/com/javalec/base/Main.java
@@ -6,16 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// kor라는 인스턴스를 만든다.
-		
-		// Field의 name에는 홍길동, phone에는 010-1234-5678을 추가하여 출력하기
-		
 		Korean kor = new Korean();
 		
-		kor.name = "홍길동";
-		kor.phone = "010-1234-5678";
+		// Field의 name에는 홍길동, phone에는 010-1234-5678을 추가하여 출력하기
+		kor.printName("홍길동", "010-1234-5678");
 		
-		System.out.println("Name : " + kor.name);
-		System.out.println("Phone : " + kor.phone);
+
 
 	}
 
--- OOP_03/src/com/javalec/function/Korean.java
+++ OOP_03/src/com/javalec/function/Korean.java
@@ -2,9 +2,9 @@ package com.javalec.function;
 
 public class Korean {
 	// Field
-	public String nation = "대한민국";
-	public String name;
-	public String phone;
+	String nation = "대한민국";
+	String name;
+	String phone;
 	
 	
 	// Constructor
@@ -13,7 +13,13 @@ public class Korean {
 	}
 	
 	
-	// Method
-	
+	// Method					Parameter
+	public void printName(String name1, String phone1) {
+		name = name1;
+		phone = phone1;
+		System.out.println("Nation : " + nation);
+		System.out.println("Name : " + name);
+		System.out.println("Phone : " + phone);
+	}
 	
 }

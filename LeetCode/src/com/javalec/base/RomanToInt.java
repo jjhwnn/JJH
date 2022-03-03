74e8d19eb1a0c3991bf750a1a5ca86c9e9af0382
jjhwnn <jeahwan174@gmail.com>
2022-03-03 21:36:41 +0900

    Create LeetCode

---
+++ LeetCode/src/com/javalec/base/RomanToInt.java
@@ -0,0 +1,20 @@
+package com.javalec.base;
+
+public class RomanToInt {
+
+	public int romanToInt(String s) {
+
+		
+		String[] romaNum = {"I", "V", "X", "L", "C", "D", "M"};
+		int[] num = {1, 5, 10, 50, 100, 500, 1000};
+		
+		for(int i = 0; i < num.length; i++) {
+			
+		}
+		
+		
+		
+		return i;
+	}
+	
+}

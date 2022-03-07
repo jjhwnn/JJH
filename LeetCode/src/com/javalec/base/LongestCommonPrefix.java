8384c4e96e292a790114c41a51ef362fb00aedf8
jjhwnn <jeahwan174@gmail.com>
2022-03-07 09:29:02 +0900

    update LongestCommonprefix

--- LeetCode/src/com/javalec/base/LongestCommonPrefix.java
+++ LeetCode/src/com/javalec/base/LongestCommonPrefix.java
@@ -5,7 +5,8 @@ public class LongestCommonPrefix {
 	public static void main(String[] args) {
 		LongestCommonPrefix prefix = new LongestCommonPrefix();
 		
-		String[] strs = {"flower","flow","flight"};
+		//String[] strs = {"flower","flow","flight"};
+		String[] strs = {"cir", "car"};
 		
 		String str = prefix.longestCommonPrefix(strs);
 		
@@ -19,41 +20,66 @@ public class LongestCommonPrefix {
     	
     	String str = "";
     	String result = "";
+
     	
-    	strs = new String[]{"flower","flow","flight"};
-    	
-    	for(int i = 0; i < strs.length-1; i++) {
+    	if(strs.length == 1) {
+    		result = strs[0];
+    	}else {
     		
-    		if(strs[i].length() < strs[i+1].length()) {
+    		for(int i = 0; i < strs.length-1; i++) {
     			
-    			for(int j = 0; j < strs[i].length(); j++) {
-    				// str배열 인덱스 문자열의 길이가 다음 인덱스 문자열보다 긴 경우
-    				// 길이가 짧은 인덱스의 문자열 길이만큼까지만 비교하기
-    				
-    				if(strs[0].charAt(j) == strs[i+1].charAt(j)) {
+    			// strs 문자열 중에 길이가 짧은 문자열길이 만큼까지만 비교
+    			
+    			if(str != "") {
+    				for(int j=0; j < str.length(); j++) {
+    					if(strs[i+1].charAt(j) == str.charAt(j)) {
+    						
+    						result += str.charAt(j);
+    					}
     					
-    					str += strs[i].charAt(j);
     				}
     				
-    			} 	
-    		}else {
-    			for(int j = 0; j < strs[i+1].length(); j++) {
+    			}	
+    			else if(strs[0].length() <= strs[i+1].length()) {
     				
-    				if(strs[0].charAt(j) == strs[i+1].charAt(j)) {
+    				for(int j = 0; j < strs[0].length(); j++) {
+    					// str배열 인덱스 문자열의 길이가 다음 인덱스 문자열보다 긴 경우
+    					// 길이가 짧은 인덱스의 문자열 길이만큼까지만 비교하기
     					
-    					str += strs[i].charAt(j);
+    					if( strs[0].charAt(j) == strs[i+1].charAt(j)) {
+    						
+    						str += strs[i].charAt(j);
+    						
+    					}
+    					
+    				} 	
+    				
+    			}else if(strs[0].length() >= strs[i+1].length()){
+    				
+    				for(int j = 0; j < strs[i+1].length(); j++) {
+    					
+    					if( strs[0].charAt(j) == strs[i+1].charAt(j)) {
+    						
+    						str += strs[i].charAt(j);
+    						
+    						
+    						
+    					}
     					
     				}
     				
     			}
     			
+    			
     		}
     		
+    		if(strs.length <= 2) {
+    			result = str;
+    		}
     	}
-    	
 
     	
-    	return str;
+    	return result;
     	
     }
 	

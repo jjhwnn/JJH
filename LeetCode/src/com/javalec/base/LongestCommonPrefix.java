7a056f3759a52544c458b0744d09a9aae6d49b97
jjhwnn <jeahwan174@gmail.com>
2022-03-07 23:00:57 +0900

    update LongestCommonPrefix

--- LeetCode/src/com/javalec/base/LongestCommonPrefix.java
+++ LeetCode/src/com/javalec/base/LongestCommonPrefix.java
@@ -14,73 +14,20 @@ public class LongestCommonPrefix {
 		
 	}
 	
-    public String longestCommonPrefix(String[] strs) {
-        // 문자열 배열을 입력받아 문자열 간에
-    	// 공통된 문자들을 출력해주는 문제
-    	
-    	String str = "";
-    	String result = "";
-
-    	
-    	if(strs.length == 1) {
-    		result = strs[0];
-    	}else {
-    		
-    		for(int i = 0; i < strs.length-1; i++) {
-    			
-    			// strs 문자열 중에 길이가 짧은 문자열길이 만큼까지만 비교
-    			
-    			if(str != "") {
-    				for(int j=0; j < str.length(); j++) {
-    					if(strs[i+1].charAt(j) == str.charAt(j)) {
-    						
-    						result += str.charAt(j);
-    					}
-    					
-    				}
-    				
-    			}	
-    			else if(strs[0].length() <= strs[i+1].length()) {
-    				
-    				for(int j = 0; j < strs[0].length(); j++) {
-    					// str배열 인덱스 문자열의 길이가 다음 인덱스 문자열보다 긴 경우
-    					// 길이가 짧은 인덱스의 문자열 길이만큼까지만 비교하기
-    					
-    					if( strs[0].charAt(j) == strs[i+1].charAt(j)) {
-    						
-    						str += strs[i].charAt(j);
-    						
-    					}
-    					
-    				} 	
-    				
-    			}else if(strs[0].length() >= strs[i+1].length()){
-    				
-    				for(int j = 0; j < strs[i+1].length(); j++) {
-    					
-    					if( strs[0].charAt(j) == strs[i+1].charAt(j)) {
-    						
-    						str += strs[i].charAt(j);
-    						
-    						
-    						
-    					}
-    					
-    				}
-    				
-    			}
-    			
-    			
-    		}
-    		
-    		if(strs.length <= 2) {
-    			result = str;
-    		}
-    	}
-
-    	
-    	return result;
-    	
-    }
+	public String longestCommonPrefix(String[] strs) {
+		  if (strs == null || strs.length == 0) {
+		    return "";
+		  }
+		  
+		  for (int i = 0; i < strs[0].length(); i++) {
+		    for (int j = 1; j < strs.length; j++) {
+		      if (i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
+		        return strs[0].substring(0, i);
+		      }
+		    }
+		  }
+		  
+		  return strs[0];
+		}
 	
 }
